6e081fcf5e5566919054555c1f1467fe8aa384c5
klekkwedge <antonov.andrey.10101@gmail.com>
2022-06-27 16:00:59 +0300

    feat: add 1 lesson

---
+++ 01 Basic Types/app.js
@@ -0,0 +1,31 @@
+"use strict";
+let a = 5;
+let b = 'sdf';
+let c = true;
+// const d = a + c
+let d = ['sdf', 'sdf', 'sdfg'];
+let e = 3;
+e = 'sdf';
+function test(a) {
+    return '';
+}
+const test2 = (a) => {
+    return a * 2;
+};
+d = d.map((x) => x.toLowerCase());
+function countCoord(coord) {
+}
+function printIt(id) {
+    if (typeof id === 'number') {
+        console.log(id.toString());
+    }
+    else if (typeof id === 'string') {
+        console.log(id.toUpperCase());
+    }
+}
+function getSum(a) {
+    if (Array.isArray(a)) {
+    }
+}
+const x = undefined;
+const z = null;
