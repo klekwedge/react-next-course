e4edbf51babe19743b8ffc09351ccbc8e97a1b52
klekkwedge <antonov.andrey.10101@gmail.com>
2022-07-02 10:21:09 +0300

    feat: add 2 lesson

---
+++ 4. Simple Components/02 First component/top-app/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Htag } from '../components';
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Htag tag="h1">Text</Htag>
+      <Htag tag="h2">Text</Htag>
+      <Htag tag="h3">Text</Htag>
+    </>
+  );
+}
